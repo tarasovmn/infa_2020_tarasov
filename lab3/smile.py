33aea8b4371199b806cd8fd1f2ecaaa8831e7bd6
Tarasov Mikhail <tarasov.mn@phystech.edu>
2020-09-22 16:41:10 +0300

    create file smile.py

--- lab3/smile.py
+++ lab3/smile.py
@@ -5,19 +5,20 @@ pygame.init()
 
 FPS = 30
 screen = pygame.display.set_mode((400, 400))
+circle(screen, (255,255,0), (200,200), 150)
+circle(screen, (255, 0, 0), (150,150), 30)
+circle(screen, (0, 0, 0), (150,150), 15)
+circle(screen, (255, 0, 0), (250,150), 30)
+circle(screen, (0, 0, 0), (250,150), 15)
 
-rect(screen, (255, 0, 255), (100, 100, 200, 200))
-rect(screen, (0, 0, 255), (100, 100, 200, 200), 5)
-polygon(screen, (255, 255, 0), [(100,100), (200,50),
-                               (300,100), (100,100)])
-polygon(screen, (0, 0, 255), [(100,100), (200,50),
-                               (300,100), (100,100)], 5)
-circle(screen, (0, 255, 0), (200, 175), 50)
-circle(screen, (255, 255, 255), (200, 175), 50, 5)
+line(screen, (0, 0, 0), (170, 140), (50, 100), 5)
+line(screen, (0, 0, 0), (230, 140), (350, 100), 5)
+line(screen, (0, 0, 0), (150, 320), (250, 320), 10)
 
 pygame.display.update()
 clock = pygame.time.Clock()
 finished = False
+
 while not finished:
     clock.tick(FPS)
     for event in pygame.event.get():
